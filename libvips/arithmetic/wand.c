7a3e884cd4d7039d43d1b1e33f288b0db33a0c07
andreagosti9 <andrea.agosti9@gmail.com>
2023-09-14 22:54:54 +0200

    Update wand.c

--- libvips/arithmetic/wand.c
+++ libvips/arithmetic/wand.c
@@ -13,102 +13,13 @@ typedef VipsUnaryClass VipsWandClass;
 
 G_DEFINE_TYPE(VipsWand, vips_wand, VIPS_TYPE_UNARY);
 
-#define LOOP(TYPE, L) \
-	{ \
-		TYPE *restrict p = (TYPE *) in[0]; \
-		TYPE *restrict q = (TYPE *) out; \
-\
-		for (x = 0; x < sz; x++) \
-			q[x] = (L) -p[x]; \
-	}
-
-#define LOOPN(TYPE) \
-	{ \
-		TYPE *restrict p = (TYPE *) in[0]; \
-		TYPE *restrict q = (TYPE *) out; \
-\
-		for (x = 0; x < sz; x++) \
-			q[x] = -1 * p[x]; \
-	}
-
-#define LOOPC(TYPE) \
-	{ \
-		TYPE *restrict p = (TYPE *) in[0]; \
-		TYPE *restrict q = (TYPE *) out; \
-\
-		for (x = 0; x < sz; x++) { \
-			q[0] = -1 * p[0]; \
-			q[1] = p[1]; \
-\
-			p += 2; \
-			q += 2; \
-		} \
-	}
-
 static void
 vips_wand_buffer(VipsArithmetic* arithmetic,
 	VipsPel* out, VipsPel** in, int width) {
-	VipsImage* im = arithmetic->ready[0];
-	const int sz = width * vips_image_get_bands(im);
 
-	int x;
-
-	switch (vips_image_get_format(im)) {
-	case VIPS_FORMAT_UCHAR:
-		LOOP(unsigned char, UCHAR_MAX);
-		break;
-	case VIPS_FORMAT_CHAR:
-		LOOPN(signed char);
-		break;
-	case VIPS_FORMAT_USHORT:
-		LOOP(unsigned short, USHRT_MAX);
-		break;
-	case VIPS_FORMAT_SHORT:
-		LOOPN(signed short);
-		break;
-	case VIPS_FORMAT_UINT:
-		LOOP(unsigned int, UINT_MAX);
-		break;
-	case VIPS_FORMAT_INT:
-		LOOPN(signed int);
-		break;
-
-	case VIPS_FORMAT_FLOAT:
-		LOOPN(float);
-		break;
-	case VIPS_FORMAT_DOUBLE:
-		LOOPN(double);
-		break;
-
-	case VIPS_FORMAT_COMPLEX:
-		LOOPC(float);
-		break;
-	case VIPS_FORMAT_DPCOMPLEX:
-		LOOPC(double);
-		break;
-
-	default:
-		g_assert_not_reached();
-	}
 }
 
-/* Save a bit of typing.
- */
-#define UC VIPS_FORMAT_UCHAR
-#define C VIPS_FORMAT_CHAR
-#define US VIPS_FORMAT_USHORT
-#define S VIPS_FORMAT_SHORT
-#define UI VIPS_FORMAT_UINT
-#define I VIPS_FORMAT_INT
-#define F VIPS_FORMAT_FLOAT
-#define X VIPS_FORMAT_COMPLEX
-#define D VIPS_FORMAT_DOUBLE
-#define DX VIPS_FORMAT_DPCOMPLEX
-
-static const VipsBandFormat vips_wand_format_table[10] = {
-	/* Band format:  UC  C  US  S  UI  I  F  X  D  DX */
-	/* Promotion: */ UC, C, US, S, UI, I, F, X, D, DX
-};
+static const VipsBandFormat vips_wand_format_table[0] = {};
 
 static void
 vips_wand_class_init(VipsWandClass* class) {
